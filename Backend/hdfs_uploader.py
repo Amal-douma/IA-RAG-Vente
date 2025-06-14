1d843f54ec8a26b0ceaa5af9366168b6fdcf34db
Douma Amal <amaldouma70@gmail.com>
2025-06-14 14:05:48 +0100

    version final

--- Backend/hdfs_uploader.py
+++ Backend/hdfs_uploader.py
@@ -1,25 +1,3 @@
-# from hdfs import InsecureClient
-# import os
-
-# def upload_to_hdfs(file_path, hdfs_url='http://localhost:9870', hdfs_dir='/reports/', user='hadoop'):
-#     try:
-#         # Connexion WebHDFS via HTTP
-#         client = InsecureClient(hdfs_url, user=user)
-        
-#         # Construire le chemin complet sur HDFS
-#         hdfs_path = os.path.join(hdfs_dir, os.path.basename(file_path))
-        
-#         # Upload avec overwrite autorisé
-#         client.upload(hdfs_path, file_path, overwrite=True)
-        
-#         print(f"✅ Fichier {file_path} envoyé vers {hdfs_path}")
-#         return hdfs_path
-#     except Exception as e:
-#         print(f"❌ Erreur lors de l'upload vers HDFS : {e}")
-#         return None
-
-# if __name__ == "__main__":
-#     upload_to_hdfs("test_report.pdf")
 from hdfs import InsecureClient
 
 def upload_to_hdfs(local_path, hdfs_path):
